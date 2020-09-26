a574ad87f5467a357e2e6b2d1a52d7f4e2081a3b
Alan Cordova <info.devalan@gmail.com>
2020-09-26 01:44:04 -0500

    SPA Control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 
 const express = require("express");
 const cors = require("cors");
@@ -27,6 +28,10 @@ app.use("/api/doctors", require("./routers/doctors"));
 app.use("/api/login", require("./routers/auth"));
 app.use("/api/search", require("./routers/searchs"));
 app.use("/api/upload", require("./routers/uploads"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Servidor corriendo en puerto " + process.env.PORT);
